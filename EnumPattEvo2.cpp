a4a7cef64773285ec33e1867a313d4ea05d88faf
AforAmpere <49594382+AforAmpere@users.noreply.github.com>
2021-10-24 22:15:44 -0400

    Added basic saving and loading, and appending

--- EnumPattEvo2.cpp
+++ EnumPattEvo2.cpp
@@ -7,21 +7,23 @@
 #include <algorithm>
 #include <array>
 #include <getopt.h>
+#include <cmath>
 
-#define PRUNE (next.p>1000)
+#define PRUNE (next.p>10000)
 #define SPECIAL_RESULT (1==2)
-#define MAXX 20
-#define MAXY 20
-#define MAXGEN 1000
+#define MAXX 5
+#define MAXY 5
+#define MAXGEN 2003
 #define MINGEN 1
 
-//Define to allow basically infinite generation depth with a slight speed sacrifice, unless the bounding box is huge, where it may still crash.
-//Don't use for now
-//#define NORECURSE
+//Define to allow basically infinite generation depth with a slight speed sacrifice, unless the bounding box is huge, where it may still crash if you don't have the RAM.
+#define NORECURSE
 
 using namespace std;
 
 ofstream outfile;
+ofstream backupfile;
+ifstream loadfile;
 
 const int translookup[512] = {0, 1, 2, 6, 1, 3, 6, 13, 2, 6, 4, 12, 5, 14, 17, 22, 51, 52, 53, 57, 52, 54, 57, 64, 53, 57, 55, 63, 56, 65, 68, 73, 2, 5, 4, 17, 6, 14, 12, 22, 7, 18, 10, 28, 18, 23, 28, 36, 53, 56, 55, 68, 57, 65, 63, 73, 58, 69, 61, 79, 69, 74, 79, 87, 1, 3, 5, 14, 8, 9, 16, 24, 6, 13, 17, 22, 16, 24, 30, 35, 52, 54, 56, 65, 59, 60, 67, 75, 57, 64, 68, 73, 67, 75, 81, 86, 5, 15, 11, 21, 16, 25, 26, 40, 18, 29, 27, 37, 31, 41, 39, 45, 56, 66, 62, 72, 67, 76, 77, 91, 69, 80, 78, 88, 82, 92, 90, 96, 2, 5, 7, 18, 5, 15, 18, 29, 4, 17, 10, 28, 11, 21, 27, 37, 53, 56, 58, 69, 56, 66, 69, 80, 55, 68, 61, 79, 62, 72, 78, 88, 4, 11, 10, 27, 17, 21, 28, 37, 10, 27, 20, 32, 27, 38, 32, 42, 55, 62, 61, 78, 68, 72, 79, 88, 61, 78, 71, 83, 78, 89, 83, 93, 6, 14, 18, 23, 16, 25, 31, 41, 12, 22, 28, 36, 26, 40, 39, 45, 57, 65, 69, 74, 67, 76, 82, 92, 63, 73, 79, 87, 77, 91, 90, 96, 17, 21, 27, 38, 30, 34, 39, 44, 28, 37, 32, 42, 39, 44, 47, 48, 68, 72, 78, 89, 81, 85, 90, 95, 79, 88, 83, 93, 90, 95, 98, 99, 1, 8, 5, 16, 3, 9, 14, 24, 5, 16, 11, 26, 15, 25, 21, 40, 52, 59, 56, 67, 54, 60, 65, 75, 56, 67, 62, 77, 66, 76, 72, 91, 6, 16, 17, 30, 13, 24, 22, 35, 18, 31, 27, 39, 29, 41, 37, 45, 57, 67, 68, 81, 64, 75, 73, 86, 69, 82, 78, 90, 80, 92, 88, 96, 3, 9, 15, 25, 9, 19, 25, 33, 14, 24, 21, 40, 25, 33, 34, 43, 54, 60, 66, 76, 60, 70, 76, 84, 65, 75, 72, 91, 76, 84, 85, 94, 14, 25, 21, 34, 24, 33, 40, 43, 23, 41, 38, 44, 41, 46, 44, 49, 65, 76, 72, 85, 75, 84, 91, 94, 74, 92, 89, 95, 92, 97, 95, 100, 6, 16, 18, 31, 14, 25, 23, 41, 17, 30, 27, 39, 21, 34, 38, 44, 57, 67, 69, 82, 65, 76, 74, 92, 68, 81, 78, 90, 72, 85, 89, 95, 12, 26, 28, 39, 22, 40, 36, 45, 28, 39, 32, 47, 37, 44, 42, 48, 63, 77, 79, 90, 73, 91, 87, 96, 79, 90, 83, 98, 88, 95, 93, 99, 13, 24, 29, 41, 24, 33, 41, 46, 22, 35, 37, 45, 40, 43, 44, 49, 64, 75, 80, 92, 75, 84, 92, 97, 73, 86, 88, 96, 91, 94, 95, 100, 22, 40, 37, 44, 35, 43, 45, 49, 36, 45, 42, 48, 45, 49, 48, 50, 73, 91, 88, 95, 86, 94, 96, 100, 87, 96, 93, 99, 96, 100, 99, 101};
 const char *intlookup[51]={"0","1c","1e","2c","2e","2k","2a","2i","2n","3c","3e","3k","3a","3i","3n","3y","3q","3j","3r","4c","4e","4k","4a","4i","4n","4y","4q","4j","4r","4t","4w","4z","5c","5e","5k","5a","5i","5n","5y","5q","5j","5r","6c","6e","6k","6a","6i","6n","7c","7e","8"};
@@ -652,11 +654,19 @@ struct mainarr
 	}
 };
 
+struct int128
+{
+	__int128 i;
+	
+};
+
 #ifndef NORECURSE
 mainarr arr[MAXGEN];
 #endif
 #ifdef NORECURSE
-mainarr* arr = nullptr;
+
+mainarr* arr = new mainarr[MAXGEN];
+
 #endif
 mainarr q;
 int xdis=100000;
@@ -672,6 +682,7 @@ bool explodeflag=1;
 bool evolveflag=0;
 bool emptyflag=0;
 bool s5flag=0;
+bool appendflag=0;
 __int128 totrules=0;
 int b1efrontend=0;
 int b2afrontend=0;
@@ -679,6 +690,8 @@ int b1e2cfrontend=0;
 int b2a2cfrontend=0;
 int b2a3ifrontend=0;
 int commonconst=0;
+string backuploc="";
+string loadloc="";
 vector<array<int,4>> speeds(0);
 
 array<array<int,MAXY+3>,MAXX+3> RLEtocelllist(string RLE)
@@ -791,6 +804,20 @@ void printint128(__int128 n)
 		n /= 10;
 	}
 	cout << s;
+}
+
+void printint128(__int128 n, ofstream& o)
+{
+	if (n == 0)  
+		o << '0';
+	char str[40] = {0};
+	char *s = str + 39;
+	while (n != 0) 
+	{
+		*--s = "0123456789"[n % 10];
+		n /= 10;
+	}
+	o << s;
 }
 
 template<typename T>
@@ -1060,8 +1087,17 @@ bool compareclist(mainarr& comparr,string j,int minx=0, int miny=0, int maxx=0, 
 	array<array<int,MAXY+3>,MAXX+3> comp = RLEtocelllist(j);
 	if(comp[2][0]==0)
 	{
-		if(comparr.p==0) return 1;
-		return 0;
+		for(int i=minx+1;i<maxx+2;i++)
+		{
+			for(int j=miny+1;j<maxy+2;j++)
+			{
+				if(comparr.clist[i][j])
+				{
+					return 0;
+				}
+			}
+		}
+		return 1;
 	}
 	if(maxx<=minx||maxy<=miny) return 0;
 	comp[0][0]=0;
@@ -1738,18 +1774,65 @@ bool checktrans(mainarr& next,int n)
 
 int nflag=0;
 
+void save(int n)
+{
+	backupfile.open(backuploc);
+	for(int i=0;i<n+1;i++)
+	{
+		printint128(postotalindex[i][0],backupfile);
+		backupfile<<endl;
+	}
+	backupfile.close();
+}
+
+int load()
+{
+	string tmp;
+	int i=0;
+	loadfile.open(loadloc);
+	while(getline(loadfile,tmp))
+	{
+		postotalindex[i][0]=(__int128)stoll(tmp);
+		//printint128(postotalindex[i][0]);
+		//cout<<endl;
+		i++;
+	}
+	// for(int j=0;j<i/2+1;j++)
+	// {
+		// combo(arr[j],trstr,postotalindex[j][0]);
+		// arr[j+1]=q;
+	// }
+	return i-1;
+}
+
 void branch(int n)
 {
-	//if (nflag>0 && pattcount>=nflag) return;
+	static int s=-1;
+	if(n==0&&loadloc!="")
+	{
+		s = load();
+	}
+	
 	array<int,103> trstr = arr[n].totrans();
 	__int128 b = intpow(trstr[102]);
 	for(__int128 i=0;i<b &&!(nflag>0 && pattcount>=nflag);i++)
 	{
-		if (nflag>0 && pattcount>=nflag) break;
-		inc++;
-		postotalindex[n][0]=i;postotalindex[n][1]=b;
-		if(inc%5000000==0)
+		if(n<=s)
 		{
+			i=postotalindex[n][0];
+			if(n==s)
+			{
+				s=-1;
+			}
+		}
+		if (nflag>0 && pattcount>=nflag) break;
+		if(n>=s)
+			inc++;
+		postotalindex[n][0]=i;postotalindex[n][1]=b;
+		if(inc%5000000==0&&inc)
+		{
+			if(backuploc!="")
+				save(n);
 			cout << "Depth: " << n+1 << ", progress: ";
 			for(int j=0; j<n+1;j++)
 			{
@@ -1825,9 +1908,10 @@ void macbipartial (string partial)
 
 int main(int argc, char **argv)
 {
-	#ifdef NORECURSE
-	mainarr* arr = new mainarr[MAXGEN];
-	#endif
+	//cout<<(int)sizeof(mainarr)*MAXGEN<<endl;
+	// #ifdef NORECURSE
+	// mainarr* arr = new mainarr[MAXGEN];
+	// #endif
 	chrono::steady_clock::time_point t1=chrono::steady_clock::now();
 	
 	int z, outflag=0,minmaxflag=0;
@@ -1854,12 +1938,15 @@ int main(int argc, char **argv)
 		{"ytrans", 1,0,'y'},
 		{"infile", 1,0,'i'},
 		{"move",0,0,'m'},
-		{"5s",0,0,'a'},
+		{"5s",0,0,'@'},
+		{"backup",1,0,'b'},
+		{"load",1,0,'l'},
+		{"append",0,0,'a'}
 	};
 	
 	int option_index=0;
 	
-	while ((z=getopt_long(argc,argv,":o:p:t:r:q:s:h:x:y:n:f:ezc:mi:a",long_options,&option_index))!=-1)
+	while ((z=getopt_long(argc,argv,":o:p:t:r:q:s:h:x:y:n:f:ezc:mi:@b:l:a",long_options,&option_index))!=-1)
 	{
 		switch(z)
 		{
@@ -1999,6 +2086,12 @@ int main(int argc, char **argv)
 					exit(0);
 				}
 				
+				if(ts=="a" || ts=="append")
+				{
+					cout << "Takes no arguments, makes EPE append to end of specified results file instead of clearing it." << endl;
+					exit(0);
+				}
+				
 				if(ts=="t" || ts=="target")
 				{
 					cout << "Enter a secondary RLE for a target pattern to have the one in -p evolve to instead, in the same format as -p. So, to look for evolutions"
@@ -2090,6 +2183,20 @@ int main(int argc, char **argv)
 				if(ts=="5s")
 				{
 					cout << "Takes no arguments. Puts EPE in 5S mode."<< endl;
+					exit(0);
+				}
+				
+				if(ts=="b" || ts=="backup")
+				{
+					cout << "Takes an backup file as an argument. Backs up program state to the file periodically."<< endl;
+					exit(0);
+				}
+				
+				if(ts=="l" || ts=="load")
+				{
+					cout << "Takes a file to load from (likely a file generated from -b) as an argument. Loads program to the state saved. Make sure to"<<
+					"use (mostly) the same parameters as were used in the original search, such as pattern and rulespace. Others may be able to be changed"<<
+					"without breaking things." << endl;
 					exit(0);
 				}
 				
@@ -2242,10 +2349,34 @@ int main(int argc, char **argv)
 				break;
 			}
 			
+			case '@':
+			{
+				//moveflag=1;
+				s5flag=1;
+				break;
+			}
+			
+			case 'b':
+			{
+				backuploc=optarg;
+				break;
+			}
+			
+			case 'l':
+			{
+				loadloc=optarg;
+				ifstream loadfile(loadloc);
+				if(!loadfile.is_open())
+				{
+					cout<<"Load file not found. Exiting."<<endl;
+					exit(0);
+				}
+				break;
+			}
+			
 			case 'a':
 			{
-				moveflag=1;
-				s5flag=1;
+				appendflag=1;
 				break;
 			}
 			
@@ -2258,6 +2389,7 @@ int main(int argc, char **argv)
 					<<"it.\n\n"
 					<< "-p is for setting the pattern, use a headerless RLE for this in quotes, i.e. -p '3o$bo$5bo'.\n\n"
 					<< "-f or --file is for setting the output file of the results.\n\n"
+					<< "-a is for allowing EPE to append results to an existing file\n\n"
 					<< "-t or --target is for setting an optional target pattern that is not the original set pattern, in\n"
 				    <<"the same format as -p.\n\n"
 					<< "-r or --prule is for setting rulerange with Macbi partial rule format. Do not use with -q or -s.\n\n"
@@ -2275,9 +2407,11 @@ int main(int argc, char **argv)
 					<< "-i or --infile is for specifying an RLE (in Golly format, without comments), and a generation count\n"
 					<<"to match the given pattern in the given rule. See -h i for more details\n\n"
 					<< "--5s to go into 5s mode.\n\n"
-					<< "There are 5 other constants to change at the top of the file, which set population bounds, maximum\n"
-					<<"horizontal bounding box, maximum vertical bounding box, maximum generation depth, and minimum\n"
-					<<"generation depth to report, respectively.\n"
+					<< "-b or --backup backs up to the file specified.\n\n"
+					<< "-l or --load loads from the file specified. Make sure to input the same search parameters.\n\n"
+					<< "There are 6 other defines to change at the top of the file, which are for special pruning and reporting\n"
+					<<"conditions, maximum horizontal bounding box, maximum vertical bounding box, maximum generation depth, and\n"
+					<<"minimum generation depth to report, respectively.\n"
 					<<"These must be set in the .cpp file, and it must be compiled again if these are changed.\n\n";
 					exit(0);
 				}
@@ -2395,8 +2529,16 @@ int main(int argc, char **argv)
 	
 	if(!outflag) file = "Output - EnumPattEvo.txt";
 	
-	outfile.open(file);
-	outfile.close();
+	if(!appendflag)
+	{
+		outfile.open(file);
+		outfile.close();
+	}
+	if(backuploc!="")
+	{
+		backupfile.open(backuploc);
+		backupfile.close();
+	}
 	//for(int c:arr[0].translist) cout<<c<<",";
 	//cout<<endl;
 	arr[0].clist = RLEtocelllist(RLE);
@@ -2472,38 +2614,51 @@ int main(int argc, char **argv)
 		branch(0);
 		#endif
 		#ifdef NORECURSE
-		//int initi=1;
-		array<int,103> trstr[MAXGEN];
-		__int128 b[MAXGEN]={0};
-		__int128 it[MAXGEN]={0};
+		int s=-1;
+		if(n==0&&loadloc!="")
+		{
+			s = load();
+		}
+		array<int,103>* trstr = new array<int,103>[MAXGEN];
 		while(n>=0)
 		{
 			outside:
 			trstr[n] = arr[n].totrans();
-			b[n] = intpow(trstr[n][102]);
-			it[n]=0;
-			while(it[n]<b[n])
+			postotalindex[n][1] = intpow(trstr[n][102]);
+			if(n<=s)
+			{
+				if(n==s)
+					s=-1;
+			}
+			else
+			{
+				postotalindex[n][0]=0;
+			}
+			while(postotalindex[n][0]<postotalindex[n][1])
 			{
 				inside:
-				if(it[n]>=b[n]) break;
+				if(postotalindex[n][0]>=postotalindex[n][1]) break;
 				if (nflag>0 && pattcount>=nflag) break;
-				inc++;
-				if(inc%5000000==0)
+				if(n>s)
+					inc++;
+				if(inc%5000000==0&&inc)
 				{
+					if(backuploc!="")
+						save(n);
 					cout << "Depth: " << n+1 << ", progress: ";
 					for(int q=0; q<n+1;q++)
 					{
 						cout << "[";
-						printint128(it[q]);
+						printint128(postotalindex[q][0]);
 						cout << "/";
-						printint128(b[q]);
+						printint128(postotalindex[q][1]);
 						cout << "],";
 						
 					}
 					cout << endl << "Patterns: " << pattcount;
 					cout << endl << endl;
 				}
-				combo(arr[n],trstr[n],it[n]);
+				combo(arr[n],trstr[n],postotalindex[n][0]);
 				bool o = checktrans(q,n);
 				if(o && n+1<MAXGEN)
 				{
@@ -2511,9 +2666,9 @@ int main(int argc, char **argv)
 					n+=1;
 					goto outside;
 				}
-				if (it[n]<b[n])
+				if (postotalindex[n][0]<postotalindex[n][1])
 				{
-					it[n]+=1;
+					postotalindex[n][0]+=1;
 				}
 			}
 			n--;
@@ -2521,18 +2676,23 @@ int main(int argc, char **argv)
 			{
 				break;
 			}
-			if (it[n]<b[n])
+			if (postotalindex[n][0]<postotalindex[n][1])
 			{
-				it[n]+=1;
+				postotalindex[n][0]+=1;
 				goto inside;
 			}
 		}	
+		delete[] trstr;
 		#endif
 	}
 	#ifdef NORECURSE
 	delete[] arr;
 	#endif
 	outfile.close();
+	if(backuploc!="")
+	{
+		backupfile.close();
+	}
 	cout << endl << endl;
 	cout << "time: " << chrono::duration_cast<chrono::duration<double>>(chrono::steady_clock::now() - t1).count() << " seconds, patterns: " << pattcount << ", inc: " << inc << endl;
 }
