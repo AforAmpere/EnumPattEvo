58a17a6a721e803d6423d704d75036fc6f02e4a0
AforAmpere <alex.pendris@gmail.com>
2024-08-26 22:32:13 -0400

    Another C2 fix

--- Rot4ReflMoore/Rot4ReflMoore.cpp
+++ Rot4ReflMoore/Rot4ReflMoore.cpp
@@ -658,18 +658,20 @@ inline void find_bounds(RulespaceRep& rulespace, CellArray& prevarray, int& low_
 				w_y=E_Y();
 				return;
 			}
-			for(int j=E_Y()-1;j>=E_Y()-low_y-1;j--)
+			for(int j=E_Y()-1;j>=E_Y()-low_y;j--)
 			{
 				for(int i=low_x;i<H_X();i++)
 				{
 					if(rulespace[prevarray.trans[i][j]][0])
 					{
-						low_y=min(low_y,E_Y()-j-1);
+						low_y=E_Y()-j-1;
 						w_y=E_Y()-2*low_y;
 						return;
 					}
 				}
 			}
+			w_y=E_Y()-2*low_y;
+			return;
 			break;
 		}
 
@@ -735,7 +737,6 @@ void evolve(RulespaceRep& rulespace, CellArray& prevarray, CellArray& newarray)
 	int pop=0;
 	find_bounds(rulespace, prevarray, low_x, low_y, dims_x, dims_y);
 	clear_grid<grid>(newarray.cells, newarray.dims_x, newarray.dims_y);
-	//clear_grid<vector<vector<bool>>>(o);
 	if(dims_x==0)
 	{
 		newarray.pop=0;
