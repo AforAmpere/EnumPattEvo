c29a03b10f40ea5b29d32ee1c817f3221e5a9d74
AforAmpere <alex.pendris@gmail.com>
2025-02-06 21:13:37 -0500

    Black-White Symmetry and bugfixes

---
+++ Rot4ReflMoore/rulespace/BWSINT.h
@@ -0,0 +1,10 @@
+#include "../Rot4ReflMoore.h"
+#define BWSINT_H
+#define TRANSCOUNT 102
+#define STATECOUNT 2
+#define RANGE 1
+
+#define LINKED 51
+#define CHECKFRONTEND
+
+extern const char *trans_name_lookup[51];
